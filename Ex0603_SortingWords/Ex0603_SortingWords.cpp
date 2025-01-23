da20c737b91d16f04c7781c4bc3df3e884c19e59
Neil <kztsly260@gmail.com>
2025-01-23 13:47:01 +0900

    <6. 버킷정렬> 단어 정렬 문제
    CLRS 연습문제 8.3-1
    
    알파벳 대문자 3글자로 이뤄진 영어 단어들을 기수 정렬(radix sort)로 정렬

--- Ex0603_SortingWords/Ex0603_SortingWords.cpp
+++ Ex0603_SortingWords/Ex0603_SortingWords.cpp
@@ -19,16 +19,83 @@ void Print(vector<T>& arr)
 
 void CountingSort(vector<string>& arr, int k, int d)
 {
-	// vector<string> temp = arr; // 복사
-	// std::fill(arr.begin(), arr.end(), "   "); // 디버깅 편의
+	vector<string> temp = arr; // 복사
+	std::fill(arr.begin(), arr.end(), "   "); // 디버깅 편의
 
 	// TODO:
+	vector<int> count(k + 1, 0);
+	for (auto a : temp)
+	{
+		count[a[d] - 'A']++;
+	}
+	for (int i = 1; i < count.size(); i++)
+	{
+		count[i] += count[i - 1];
+	}
+
+	for (int i = arr.size() - 1; i >= 0; i--)
+	{
+		arr[count[temp[i][d] - 'A'] - 1] = temp[i];
+		count[temp[i][d] - 'A']--;
+
+		cout << "Count: ";
+		Print(count);
+
+		cout << "Output: ";
+		Print(arr);
+	}
 }
 
 void RadixSort(vector<string>& arr)
 {
 	// TODO:
+	int k = 25;
+
+	for (int d = 2; d >= 0; d--)
+	{
+		cout << "Digit = " << d << endl;
+		CountingSort(arr, k, d);
+	}
 }
+
+// 정답:
+//void CountingSort(vector<string>& arr, int k, int d)
+//{
+//	vector<string> temp = arr; // 복사
+//
+//	vector<int> count(k + 1, 0);
+//	for (auto a : temp) count[a[d] - 'A'] += 1;
+//	for (int i = 1; i < count.size(); i++)
+//		count[i] += count[i - 1];
+//
+//	for (int i = arr.size() - 1; i >= 0; i--)
+//	{
+//		arr[count[temp[i][d] - 'A'] - 1] = temp[i];
+//		count[temp[i][d] - 'A']--;
+//
+//		cout << "Count: ";
+//		Print(count);
+//
+//		cout << "Output: ";
+//		Print(arr);
+//	}
+//}
+//
+//void RadixSort(vector<string>& arr)
+//{
+//	int k = 'Z' - 'A';
+//	// 아래 CountingSort()에서 사용할 가장 작은 인덱스(arr에서 가장 작은 값)은 0
+//	// 가장 큰 인덱스(arr에서 가장 큰 값)이 k
+//	// 비교: 알파벳 개수는 'Z' - 'A' + 1
+//
+//	int m = 3; // 전부 3글자라고 가정
+//
+//	for (int d = m - 1; d >= 0; d--)
+//	{
+//		cout << "Digit = " << d << endl;
+//		CountingSort(arr, k, d);
+//	}
+//}
 
 int main()
 {
